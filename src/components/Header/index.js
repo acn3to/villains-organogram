d6b203ca88d55327da72295f1fd8664a2080db87
Arnaldo Neto <costanetoow@gmail.com>
2023-01-17 01:21:43 -0300

    :sparkles: creates header component
    Additionally removes unnecessary files

---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+  return (
+    <header className="banner">
+      <div className="banner-text">
+        <h1>Vilões</h1>
+        <h3>Organograma</h3>
+      </div>
+    </header>
+  )
+}
+
+export default Header
