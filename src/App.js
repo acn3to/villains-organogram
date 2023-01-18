43536a04f9165dce80190657bf4e1182f893c523
Arnaldo Neto <costanetoow@gmail.com>
2023-01-18 06:35:06 -0300

    :sparkles: creates text-field component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import Header from './components/Header'
+import TextField from './components/TextField'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <TextField label="Nome" placeholder="Digite o nome" />
+      <TextField label="Anime" placeholder="Digite o anime" />
+      <TextField label="Imagem" placeholder="Selecione um arquivo" />
+      <TextField label="Organização" placeholder="Digite a organização" />
     </div>
   )
 }
---
+++ src/components/TextField/index.js
@@ -0,0 +1,14 @@
+import './TextField.css'
+
+import React from 'react'
+
+const TextField = (props) => {
+  return (
+    <div className="text-field">
+      <label htmlFor="">{props.label}</label>
+      <input type="text" placeholder={props.placeholder} />
+    </div>
+  )
+}
+
+export default TextField
